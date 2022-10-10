4a70325613a6cdf16526ff990146b464ac2f3c26
5011286mountjoy <115504868+5011286mountjoy@users.noreply.github.com>
2022-10-10 13:44:15 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-//tewst
+effects.starField.startScreenEffect()
+let player = sprites.create(assets.image`Placeholder`, SpriteKind.Player)
+controller.moveSprite(player, 75, 0)
+player.setPosition(75, 100)
