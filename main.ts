f64170f1c9de35ec1f706c4415cac4a9d20df8c3
5011286mountjoy <115504868+5011286mountjoy@users.noreply.github.com>
2022-10-10 13:45:19 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-effects.starField.startScreenEffect()
+effects.starField.startScreenEffect() //making stars
 let player = sprites.create(assets.image`Placeholder`, SpriteKind.Player)
 controller.moveSprite(player, 75, 0)
 player.setPosition(75, 100)
